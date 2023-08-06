6fcc1773adec8bc12323bbc49e6b91c912b20943
ivansamusev <ivansamusev@gmail.com>
2023-08-06 20:53:23 +0300

    events

---
+++ src/js/createHTML.js
@@ -0,0 +1,24 @@
+const ul = document.querySelector('.cards');
+
+function createHTML(eventData) {
+    eventData.map(({name, dates:{start}, images}) => {
+    const markup = `
+    <div class='container'>
+    <div class='cards'>
+    
+    <li class="card-item">
+    <img class="card-image" src=${images[0].url} alt="" loading="lazy">
+    <div class="information">
+    <p class='name'>${name}</p>
+    <p class='date'>${start.localDate}</p
+    </div>
+    </li>
+    </div>
+    </div>`
+
+    ul.insertAdjacentHTML("beforeend", markup);
+    });
+};
+
+
+export {createHTML}
