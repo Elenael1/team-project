38c6a169aee18fbc4f48c715e3c9499bc97d7978
Elena <115032773+Elenael1@users.noreply.github.com>
2023-08-11 11:46:57 +0200

    js

--- src/js/request.js
+++ src/js/request.js
@@ -7,7 +7,7 @@ const pages = 1;
 
 window.addEventListener('load', onLoad)
 async function onLoad() {
-    const data = await fetch(`${API_URL}?countryCode=US&apikey=${API_KEY}&page=${pages}&limit=20`)
+    const data = await fetch(`${API_URL}?countryCode=US&apikey=${API_KEY}&page=${pages}&limit=10`)
     .then((responce)=>{
         return responce.json();
     });
