43e208be76e3e2302f28be75af8b2efedbbfeed0
JareJarekus <115168159+Jaroslav866@users.noreply.github.com>
2023-08-24 19:52:17 +0300

    modal
    close button added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,17 @@
 const cards = document.querySelector('.cards');
-const modal = document.querySelector('.modal')
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
 
-cards.addEventListener('click', modalClick);
+cards.addEventListener('click', openModal);
+close.addEventListener('click', closeModalBtn)
 
-function modalClick (e){
+function openModal (e){
     modal.style.left = '50%';
+    modal.style.transform = 'translate(-50%, 20%)';
+    modal.style.transition = '0.5s';
+}
+function closeModalBtn (e){
+    modal.style.left = '150%';
     modal.style.transform = 'translate(-50%, 20%)';
     modal.style.transition = '0.5s';
 }
