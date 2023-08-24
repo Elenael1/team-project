9027434b28f4fe446c09937938a98a8f149735dc
JareJarekus <115168159+Jaroslav866@users.noreply.github.com>
2023-08-24 20:45:29 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,9 +16,8 @@ function closeModalBtn (e){
     modal.style.transform = 'translate(-50%, 20%)';
     modal.style.transition = '0.5s';
 }
-function closeModalOnKey({ key, code}){
+function closeModalOnKey({key, code}){
     if(code === "Escape"){
-        console.log("Я натиснув Escape");
         modal.style.left = '150%';
         modal.style.transform = 'translate(-50%, 20%)';
         modal.style.transition = '0.5s';
