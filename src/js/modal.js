f81f52fcad56998d283a09987b76e9f0ccc49652
JareJarekus <115168159+Jaroslav866@users.noreply.github.com>
2023-08-28 16:31:34 +0300

    modal
    not correct blur

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,6 +1,7 @@
 const cards = document.querySelector('.cards');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
+const blur = document.querySelector('.blur');
 
 cards.addEventListener('click', openModal);
 close.addEventListener('click', closeModalBtn)
@@ -9,17 +10,20 @@ addEventListener("keydown", closeModalOnKey);
 function openModal (e){
     modal.style.left = '50%';
     modal.style.transform = 'translate(-50%, 20%)';
-    modal.style.transition = '0.5s';
+    modal.style.transition = '0.3s';
+    blur.style.zIndex = '11';
 }
 function closeModalBtn (e){
-    modal.style.left = '150%';
+    modal.style.left = '200%';
     modal.style.transform = 'translate(-50%, 20%)';
-    modal.style.transition = '0.5s';
+    modal.style.transition = '0.3s';
+    blur.style.zIndex = '-11';
 }
 function closeModalOnKey({key, code}){
     if(code === "Escape"){
-        modal.style.left = '150%';
+        modal.style.left = '200%';
         modal.style.transform = 'translate(-50%, 20%)';
-        modal.style.transition = '0.5s';
+        modal.style.transition = '0.3s';
+        blur.style.zIndex = '-11';
     }
 }
