4a3e808f6bf469e08731c4e03b89d1a2e8d963a0
JareJarekus <115168159+Jaroslav866@users.noreply.github.com>
2023-08-28 17:21:50 +0300

    modal
    correct blur power & color

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,6 @@
 const cards = document.querySelector('.cards');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
-const blur = document.querySelector('.blur');
 
 cards.addEventListener('click', openModal);
 close.addEventListener('click', closeModalBtn)
@@ -10,23 +9,20 @@ addEventListener("keydown", closeModalOnKey);
 function openModal (e){
     modal.style.left = '50%';
     modal.style.transform = 'translate(-50%, 20%)';
-    modal.style.transition = '0.5s';
+    modal.style.transition = '0.3s';
     document.querySelector('.modal-overlay').style.display = 'block';
 }
-
 function closeModalBtn (e){
-    modal.style.left = '150%';
+    modal.style.left = '200%';
     modal.style.transform = 'translate(-50%, 20%)';
-    modal.style.transition = '0.5s';
+    modal.style.transition = '0.3s';
     document.querySelector('.modal-overlay').style.display = 'none';
 }
-
 function closeModalOnKey({key, code}){
     if(code === "Escape"){
         modal.style.left = '200%';
         modal.style.transform = 'translate(-50%, 20%)';
         modal.style.transition = '0.3s';
         document.querySelector('.modal-overlay').style.display = 'none';
-
     }
 }
