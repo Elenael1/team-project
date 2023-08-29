f61a9c523b622ab1505b1a95344ce3ab5b0148d9
JareJarekus <115168159+Jaroslav866@users.noreply.github.com>
2023-08-29 15:48:34 +0300

    modal
    closes by clicking at overlay

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,28 +1,36 @@
 const cards = document.querySelector('.cards');
 const modal = document.querySelector('.modal');
 const close = document.querySelector('.close');
+const overlay = document.querySelector('.modal-overlay');
 
 cards.addEventListener('click', openModal);
 close.addEventListener('click', closeModalBtn)
 addEventListener("keydown", closeModalOnKey);
+overlay.addEventListener('click', closeModalOnOverlay)
 
 function openModal (e){
     modal.style.left = '50%';
     modal.style.transform = 'translate(-50%, 20%)';
     modal.style.transition = '0.3s';
-    document.querySelector('.modal-overlay').style.display = 'block';
+    overlay.style.display = 'block';
 }
 function closeModalBtn (e){
     modal.style.left = '200%';
     modal.style.transform = 'translate(-50%, 20%)';
     modal.style.transition = '0.3s';
-    document.querySelector('.modal-overlay').style.display = 'none';
+    overlay.style.display = 'none';
 }
 function closeModalOnKey({key, code}){
     if(code === "Escape"){
         modal.style.left = '200%';
         modal.style.transform = 'translate(-50%, 20%)';
         modal.style.transition = '0.3s';
-        document.querySelector('.modal-overlay').style.display = 'none';
+        overlay.style.display = 'none';
     }
+}
+function closeModalOnOverlay(e){
+    modal.style.left = '200%';
+    modal.style.transform = 'translate(-50%, 20%)';
+    modal.style.transition = '0.3s';
+    overlay.style.display = 'none';
 }
